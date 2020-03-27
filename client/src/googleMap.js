138ff1cb7701ac5f7db556bf1da7a811f24f5369
Kelley Neubauer <kelleyneubauer@gmail.com>
2020-03-27 13:33:07 -0700

    Changed location of map to Oregon State University

--- client/src/googleMap.js
+++ client/src/googleMap.js
@@ -1,10 +1,11 @@
 // Initialize and add the map
 function initMap() {
-	// The location of Uluru
-	var uluru = {lat: -25.344, lng: 131.036};
-	// The map, centered at Uluru
+	// The location of OSU
+	var oregonState = {lat: 44.564466, lng: -123.279528};
+	// The map, centered at OSU
 	var map = new google.maps.Map(
-		document.getElementById('map'), {zoom: 4, center: uluru});
+		document.getElementById('map'), {zoom: 4, center: oregonState});
 	// The marker, positioned at Uluru
-	var marker = new google.maps.Marker({position: uluru, map: map});
+	var marker = new google.maps.Marker({position: oregonState, map: map});
+
   }
