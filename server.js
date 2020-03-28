62a937890f4710adc6d742daf84c4ab279f4d64f
Lexi C <37921314+DysL3xiA@users.noreply.github.com>
2020-03-27 20:51:47 -0700

    testing adding static files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('/client/public'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 // postgres database set up
