684109b801c75ab1ed4d4738a5efba3f988fa93b
Lexi C <37921314+DysL3xiA@users.noreply.github.com>
2020-03-28 08:41:13 -0700

    functioning post request on node; returns lat long of address submitted

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Client = require("@googlemaps/google-maps-services-js").Client;
 var app = express();
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,21 +38,51 @@ app.post ("/", async (request, response) => {
   const email = request.body.email;
   const address = request.body.address;
   console.log(address);
+  var latitude;
+  var longitude;
+  var city;
+  var state;
+  var country;
+
+  var context = {}; //for returning to post request
+
   const client = await new Client({address});
 
-client
-  .geocode({
-    params: {
-      address: `${address}`,
-      key: process.env.GOOGLE_MAPS_API_KEY
-    },
-    timeout: 1000 // milliseconds
-  })
-  .then(r => {
-    console.log(r.data.results.geometry.location.lat);
-  })
-  .catch(e => {
-    console.log(e);
-  });
+  client
+    .geocode({
+      params: {
+        address: `${address}`,
+        key: process.env.GOOGLE_MAPS_API_KEY
+      },
+      timeout: 1000 // milliseconds
+    })
+    .then(r => {
+      console.log("r.data ", r.data);
+      latitude = r.data.results[0].geometry.location.lat;
+      longitude = r.data.results[0].geometry.location.lng;
+      city = r.data.results[0].address_components[4].long_name;
+      state = r.data.results[0].address_components[6].long_name;
+      country = r.data.results[0].address_components[7].long_name;
+
+      console.log("latitude ", latitude, "longitude ", longitude);
+      console.log("city ", city, "state ", state, "country ", country);
+
+      var results = [];
+
+      results.push({"latitude": latitude,
+        "longitude": longitude,
+        "city": city,
+        "state": state,
+        "country": country});
+
+      context.results = results;
+
+      console.log("context ", context);
+
+      response.send(context);
+    })
+    .catch(e => {
+      console.log("e ", e);
+    });
 
 })
