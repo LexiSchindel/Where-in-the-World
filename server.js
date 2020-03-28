4f4fa9a936e9efcec05dcdb46ce74b9feb66701f
Lexi C <37921314+DysL3xiA@users.noreply.github.com>
2020-03-27 18:12:12 -0700

    adding database connection

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// postgres database set up
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+// create table
+const query = client.query(
+    'CREATE TABLE maps(id SERIAL PRIMARY KEY, address VARCHAR(100) not null, email VARCHAR(40))');
+  query.on('end', () => { client.end(); });
+
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
