a802c4ea917da45d69d5f0f875c23235be5d473c
Lexi C <37921314+DysL3xiA@users.noreply.github.com>
2020-03-28 20:06:47 -0700

    added in a table for stats page

--- client/src/stats.js
+++ client/src/stats.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', cityTable()); 
 
-const table = document.getElementsByClassName("cityTable")
+const table = document.getElementById("cityTable")
 
 //get the data from url provided; return the parsed text
 function getData(url){
@@ -54,34 +54,3 @@ function initTable(data){
         tBody.appendChild(row);
     }
 }
-
-{/* <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table> */}
